49821ec12ae5f26ee3b558812e6763c993b34487
peppapighs <ptk.prasertsuk@gmail.com>
2025-01-24 19:22:58 +0800

    :sparkles: add profile selector to the sidebar

--- src/components/configurator/sidebar.tsx
+++ src/components/configurator/sidebar.tsx
@@ -1,8 +1,15 @@
 "use client"
 
+import { NUM_PROFILES } from "@/constants/devices"
 import { displayUint16 } from "@/lib/utils"
-import { ChevronsUpDown } from "lucide-react"
+import {
+  RadioGroup,
+  RadioGroupIndicator,
+  RadioGroupItem,
+} from "@radix-ui/react-radio-group"
+import { ChevronsUpDown, Circle } from "lucide-react"
 import { useRouter } from "next/navigation"
+import { useConfigurator } from "../configurator-provider"
 import { useDevice } from "../device-provider"
 import { Button } from "../ui/button"
 import {
@@ -15,6 +22,7 @@ import {
 import { ScrollArea } from "../ui/scroll-area"
 
 export function ConfiguratorSidebar() {
+  const { profileNum, setProfileNum } = useConfigurator()
   const { metadata, isDemo, disconnect } = useDevice()
 
   const router = useRouter()
@@ -31,7 +39,7 @@ export function ConfiguratorSidebar() {
       <div className="flex flex-col gap-4 p-4">
         <DropdownMenu>
           <DropdownMenuTrigger asChild>
-            <Button variant="outline" className="h-auto px-3">
+            <Button variant="outline" className="h-auto px-4 py-3">
               <div className="grid flex-1 text-left">
                 <p className="text-xs text-muted-foreground">Device</p>
                 <span className="truncate">{metadata.name}</span>
@@ -56,6 +64,29 @@ export function ConfiguratorSidebar() {
             </DropdownMenuItem>
           </DropdownMenuContent>
         </DropdownMenu>
+        <div className="flex flex-col gap-3 rounded-md border bg-card p-4 shadow-sm">
+          <p className="font-semibold leading-none">Profiles</p>
+          <RadioGroup
+            value={profileNum.toString()}
+            onValueChange={(value) => setProfileNum(Number(value))}
+            className="grid gap-1"
+          >
+            {[...Array(NUM_PROFILES)].map((_, i) => (
+              <RadioGroupItem
+                key={i}
+                value={i.toString()}
+                className="relative flex items-center rounded-sm py-1.5 pl-8 pr-2 text-sm outline-none transition-colors hover:bg-accent hover:text-accent-foreground data-[state=checked]:bg-accent data-[state=checked]:text-accent-foreground"
+              >
+                <span className="absolute left-2 flex size-3.5 items-center justify-center">
+                  <RadioGroupIndicator>
+                    <Circle className="size-2 fill-current" />
+                  </RadioGroupIndicator>
+                </span>
+                Profile {i}
+              </RadioGroupItem>
+            ))}
+          </RadioGroup>
+        </div>
       </div>
     </ScrollArea>
   )
