2a64a0ab8430e8cf2fe8067cb19a39a873d522c6
Ramon Imbao <ramonimbao@gmail.com>
2025-06-15 09:37:44 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
   output: "export", // Enable static HTML export
-  basePath: "/hmkconf", // Needed for GitHub Pages (project repo)
   trailingSlash: true, // Ensures folder structure matches static hosting
 }
 
