eed30d7e7c55cd242e78302b62ec63574183fb2c
Ramon Imbao <ramonimbao@gmail.com>
2025-07-28 17:56:31 +0800

    Add useEffect again

--- src/components/app-configurator.tsx
+++ src/components/app-configurator.tsx
@@ -19,7 +19,7 @@ import { useHMKDevice } from "@/hooks/use-hmk-device"
 import { createConfigurator } from "@/lib/create-configurator"
 import { isWebHIDSupported } from "@/lib/utils"
 import { useQueryClient } from "@tanstack/react-query"
-import { useLayoutEffect } from "react"
+import { useEffect, useLayoutEffect } from "react"
 import { Configurator } from "./configurator/configurator"
 import { ConfiguratorLayout } from "./configurator/layout"
 import { ConfiguratorProvider } from "./providers/configurator-provider"
@@ -42,6 +42,10 @@ export function AppConfigurator() {
       hmkDevice.connect()
     }
   }, [hmkDevice, queryClient, reset])
+
+  useEffect(() => {
+    setWebHIDSupported(isWebHIDSupported())
+  }, [])
 
   return (
     <ConfiguratorProvider configurator={useAppConfigurator()}>
