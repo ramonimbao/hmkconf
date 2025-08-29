bcc43c0a063299305284f2104ea36874cd8d2b7c
Ramon Imbao <ramonimbao@gmail.com>
2025-08-29 13:42:13 +0800

    Set images to unoptimized

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from "next"
 const nextConfig: NextConfig = {
   output: "export", // Enable static HTML export
   trailingSlash: true, // Ensures folder structure matches static hosting
+
+  images: {
+    unoptimized: true,
+  },
 }
 
 export default nextConfig
