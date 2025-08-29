e09188703ac703ceacfe32950b2db4c432e90e8a
Ramon Imbao <ramonimbao@gmail.com>
2025-08-29 13:35:27 +0800

    Remove semicolon

--- app/manifest.ts
+++ app/manifest.ts
@@ -15,7 +15,7 @@
 
 import type { MetadataRoute } from "next"
 
-export const dynamic = 'force-static';
+export const dynamic = 'force-static'
 
 export default function manifest(): MetadataRoute.Manifest {
   return {
