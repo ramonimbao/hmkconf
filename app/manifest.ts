fc1af0a04bf1b1be71843eeeb4d85f27db855e43
Ramon Imbao <ramonimbao@gmail.com>
2025-08-29 13:34:28 +0800

    Set force-static output in manifest

--- app/manifest.ts
+++ app/manifest.ts
@@ -15,6 +15,8 @@
 
 import type { MetadataRoute } from "next"
 
+export const dynamic = 'force-static';
+
 export default function manifest(): MetadataRoute.Manifest {
   return {
     name: "hmkconf",
