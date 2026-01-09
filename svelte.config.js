674fe1458c7e0e2e9654d78c0f823ce583ba5f7e
Ramon Imbao <ramonimbao@gmail.com>
2026-01-09 17:37:31 +0800

    static site deploy

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,11 @@
-import adapter from "@sveltejs/adapter-vercel"
-import { vitePreprocess } from "@sveltejs/vite-plugin-svelte"
+import adapter from '@sveltejs/adapter-static'
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-  preprocess: vitePreprocess(),
-  kit: { adapter: adapter() },
+export default {
+  kit: {
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: null,
+    }),
+  },
 }
-
-export default config
